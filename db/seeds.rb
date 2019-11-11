6af49be043799080dc95aeba46144bb1fa5cd8ac
Oscar Lindberg <oscar.hewitt.lindberg@gmail.com>
2019-11-11 17:58:01 +0100

    wip - added faker and started on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# first festroy all the instances of all mdels
+
+
+oscar = User.create!()
+
+jules = User.create
+
+carla = User.create
+#add comments 'creating users'
+
+#20 times
+# for 7 use each user
+Actor.create()
+
+#comment 'creating actors'
+
+# fake users =
+# name = User
+# 7 actors each
+
+# comment 'creating bookings'
+
+# create a few bookings
+# for you, carla and jules' actors
+
+#comment 'finishing'
